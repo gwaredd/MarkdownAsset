6338d3daecd1b84c76b74e2eb7f1ff71ea19817b
gwaredd <gwaredd@hotmail.com>
2024-01-11 12:40:58 +0000

    added build rules for light and dark flavours

--- Viewer/vite.config.js
+++ Viewer/vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
 import { viteSingleFile } from "vite-plugin-singlefile"
+import dotenv from 'dotenv';
+
+const flavour = process.env.FLAVOUR || 'dark';
+dotenv.config({ path: `.env.${flavour.trim()}` });
 
 export default defineConfig({
   plugins: [
@@ -8,5 +13,17 @@ export default defineConfig({
     viteSingleFile()
   ],
   optimizeDeps: {
+  },
+  resolve: {
+    alias: [
+        {
+          find: 'App.css',
+          replacement: path.resolve( __dirname, `./src/${process.env.VITE_CSS_PATH}` ),
+        },
+        {
+          find: 'Highlight.css',
+          replacement: `highlight.js/styles/${process.env.VITE_HIGHLIGHT_THEME}.css`
+        },
+    ],
   }
 })
