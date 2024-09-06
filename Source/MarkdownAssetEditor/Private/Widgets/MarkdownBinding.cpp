aa983f648a880ebf883f2b996483f7b044f8c007
MaxiCabaas <cabanas.maximiliano@gmail.com>
2024-09-05 23:06:51 -0300

    Added support for links to Assets and C++ files
    Now MD files can have links to assets, C++ files or even specific C++ functions.

--- Source/MarkdownAssetEditor/Private/Widgets/MarkdownBinding.cpp
+++ Source/MarkdownAssetEditor/Private/Widgets/MarkdownBinding.cpp
@@ -4,3 +4,8 @@ void UMarkdownBinding::OpenURL( FString URL )
 {
     FPlatformProcess::LaunchURL( *URL, nullptr, nullptr );
 }
+
+void UMarkdownBinding::OpenAsset( FString URL )
+{
+    GEditor->GetEditorSubsystem<UAssetEditorSubsystem>()->OpenEditorForAsset(URL);
+}
