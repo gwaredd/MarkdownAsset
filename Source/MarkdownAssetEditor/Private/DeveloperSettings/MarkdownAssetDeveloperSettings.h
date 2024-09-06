ab9cca1e3aeba2d789125c7a60eb444ff43d825b
MaxiCabaas <cabanas.maximiliano@gmail.com>
2024-09-06 16:31:52 -0300

    Implemented Button in toolbar and developer settings
    Now a project main markdown file can be set in the custom developer settings to be open from a custom button in the toolbar

---
+++ Source/MarkdownAssetEditor/Private/DeveloperSettings/MarkdownAssetDeveloperSettings.h
@@ -0,0 +1,16 @@
+﻿// Copyright (C) 2024 El Burro Games - All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DeveloperSettingsBackedByCVars.h"
+#include "MarkdownAssetDeveloperSettings.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MARKDOWNASSETEDITOR_API UMarkdownAssetDeveloperSettings : public UDeveloperSettingsBackedByCVars
+{
+	GENERATED_BODY()
+};
